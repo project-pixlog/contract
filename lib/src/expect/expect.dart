461b2113dc0af4eed2f78c6a686c2210aead8d8a
Kaio Delphino <github@kaiodelphino.com>
2024-02-21 22:31:53 -0300

    refac: remove test requirement & update expect api

--- lib/src/expect/expect.dart
+++ lib/src/expect/expect.dart
@@ -46,13 +46,8 @@ typedef ErrorFormatter = String Function(Object? actual, Matcher matcher,
 /// the test doesn't complete until the matcher has either matched or failed. If
 /// you want to wait for the matcher to complete before continuing the test, you
 /// can call [expectLater] instead and `await` the result.
-void expect(dynamic actual, dynamic matcher,
-    {String? reason,
-    Object? /* String|bool */ skip,
-    @Deprecated('Will be removed in 0.13.0.') bool verbose = false,
-    @Deprecated('Will be removed in 0.13.0.') ErrorFormatter? formatter}) {
-  _expect(actual, matcher,
-      reason: reason, skip: skip, verbose: verbose, formatter: formatter);
+void expect(dynamic actual, dynamic matcher, {String? reason}) {
+  _expect(actual, matcher, reason: reason);
 }
 
 /// Just like [expect], but returns a [Future] that completes when the matcher
@@ -66,41 +61,26 @@ void expect(dynamic actual, dynamic matcher,
 ///
 /// If the matcher fails asynchronously, that failure is piped to the returned
 /// future where it can be handled by user code.
-Future expectLater(dynamic actual, dynamic matcher,
-        {String? reason, Object? /* String|bool */ skip}) =>
-    _expect(actual, matcher, reason: reason, skip: skip);
+Future expectLater(dynamic actual, dynamic matcher, {String? reason}) =>
+    _expect(actual, matcher, reason: reason);
+
+String formatter(
+  Object? actual,
+  Object? matcher,
+  String? reason,
+  Map<dynamic, dynamic> matchState,
+) {
+  var mismatchDescription = StringDescription();
+  (matcher as Matcher)
+      .describeMismatch(actual, mismatchDescription, matchState, false);
+
+  return formatFailure(matcher, actual, mismatchDescription.toString(),
+      reason: reason);
+}
 
 /// The implementation of [expect] and [expectLater].
-Future _expect(Object? actual, Object? matcher,
-    {String? reason, skip, bool verbose = false, ErrorFormatter? formatter}) {
-  final test = TestHandle.current;
-  formatter ??= (actual, matcher, reason, matchState, verbose) {
-    var mismatchDescription = StringDescription();
-    matcher.describeMismatch(actual, mismatchDescription, matchState, verbose);
-
-    return formatFailure(matcher, actual, mismatchDescription.toString(),
-        reason: reason);
-  };
-
-  if (skip != null && skip is! bool && skip is! String) {
-    throw ArgumentError.value(skip, 'skip', 'must be a bool or a String');
-  }
-
+Future _expect(Object? actual, Object? matcher, {String? reason}) {
   matcher = wrapMatcher(matcher);
-  if (skip != null && skip != false) {
-    String message;
-    if (skip is String) {
-      message = 'Skip expect: $skip';
-    } else if (reason != null) {
-      message = 'Skip expect ($reason).';
-    } else {
-      var description = StringDescription().addDescriptionOf(matcher);
-      message = 'Skip expect ($description).';
-    }
-
-    test.markSkipped(message);
-    return Future.sync(() {});
-  }
 
   if (matcher is AsyncMatcher) {
     // Avoid async/await so that expect() throws synchronously when possible.
@@ -117,15 +97,11 @@ Future _expect(Object? actual, Object? matcher,
     if (result is String) {
       fail(formatFailure(matcher, actual, result, reason: reason));
     } else if (result is Future) {
-      final outstandingWork = test.markPending();
       return result.then((realResult) {
         if (realResult == null) return;
         fail(formatFailure(matcher as Matcher, actual, realResult as String,
             reason: reason));
-      }).whenComplete(
-          // Always remove this, in case the failure is caught and handled
-          // gracefully.
-          outstandingWork.complete);
+      });
     }
 
     return Future.sync(() {});
@@ -139,7 +115,7 @@ Future _expect(Object? actual, Object? matcher,
   } catch (e, trace) {
     reason ??= '$e at $trace';
   }
-  fail(formatter(actual, matcher as Matcher, reason, matchState, verbose));
+  fail(formatter(actual, matcher as Matcher, reason, matchState));
 }
 
 /// Convenience method for throwing a new [TestFailure] with the provided

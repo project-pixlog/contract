4e91449faea5916875da1277740352c4a7011df9
Kaio Delphino <dash.zet@gmail.com>
2024-04-03 23:59:37 -0300

    chore: update functions documentation
    Add spaces between paragraphs to improve redability

--- lib/contract.dart
+++ lib/contract.dart
@@ -22,14 +22,15 @@ export 'src/util.dart';
 
 abstract class Contract {
   static bool get verbose => Listeners.verbose;
+
   /// Whether a clause evaluation should throw an exception
   static bool get shouldThrow => Listeners.shouldThrow;
 
   /// Configures [verbose] and [shouldThrow] modes.
-  ///
-  /// When [verbose] is set to true, a not null StackTrace will be provided for 
+  ///  
+  /// When [verbose] is set to true, a not null StackTrace will be provided for
   /// listeners together with [reason].
-  /// When [shouldThrow] is set to true, a [ContractClauseBroken] exception is 
+  /// When [shouldThrow] is set to true, a [ContractClauseBroken] exception is
   /// thrown whenever a clause is broken.
   static void init({
     bool? verbose,
@@ -50,13 +51,13 @@ abstract class Contract {
   static void dispose() => Listeners.dispose();
 
   /// Assert that [actual] matches [matcher]. Throws if don't.
-  ///
+  ///  
   /// [matcher] can be a value in which case it will be wrapped in an
   /// [equals] matcher.
-  ///
+  ///  
   /// If the assertion fails [reason] is send to [listeners].
   /// If [verbose] mode is on, StackTrace.current is send to [listeners] too.
-  ///
+  ///  
   /// Certain matchers, like [completion] and [throwsA], either match or fail
   /// asynchronously. When you use [softClause] with these matchers, it ensures
   /// that the test doesn't complete until the matcher has either matched or
@@ -67,13 +68,13 @@ abstract class Contract {
 
   /// Just like [clause], but returns a [Future] that completes when the matcher
   /// has finished matching.
-  ///
+  ///  
   /// For the [completes] and [completion] matchers, as well as [throwsA] and
   /// related matchers when they're matched against a [Future], the returned
   /// future completes when the matched future completes. For the [prints]
   /// matcher, it completes when the future returned by the callback completes.
   /// Otherwise, it completes immediately.
-  ///
+  ///  
   /// If the matcher fails asynchronously, that failure is piped to the returned
   /// future where it can be handled by user code.
   static Future asyncClause(dynamic actual, dynamic matcher) async =>
